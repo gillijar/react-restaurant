2ec681adaa5af15c448e382c47aab83830248761
Jarrod Gilliland <gillijar@gmail.com>
2021-07-22 14:48:04 -0400

    starting setting up cart styles and functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,26 @@
 import { useSelector } from "react-redux";
 
 import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
+  const totalCartPrice = useSelector((state) => state.cart.totalCartPrice);
 
   return (
-    <section>
-      {cartItems.length === 0 && <p>Empty cart</p>}
-      {cartItems.length > 0 &&
-        cartItems.map((item) => (
-          <CartItem name={item.name} price={item.price} />
-        ))}
+    <section className={classes.cart}>
+      <div className={classes["cart-container"]}>
+        {cartItems.length === 0 && <p>Empty cart</p>}
+        {cartItems.length > 0 &&
+          cartItems.map((item) => (
+            <CartItem
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
+            />
+          ))}
+        <p>${totalCartPrice.toFixed(2)}</p>
+      </div>
     </section>
   );
 };
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,9 +1,22 @@
+import classes from "./CartItem.module.css";
+
 const CartItem = (props) => {
   return (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.price}</p>
-    </div>
+    <figure className={classes["cart-item"]}>
+      <div className={classes["cart-item__details"]}>
+        <p className={classes["cart-item__details--multi"]}>
+          x{props.quantity}
+        </p>
+        <p>{props.name}</p>
+      </div>
+      <div>
+        <div className={classes["cart-item__price"]}>
+          <p>${props.price}</p>
+        </div>
+        <button>-</button>
+        <button>+</button>
+      </div>
+    </figure>
   );
 };
 
