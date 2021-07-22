30488c6226916ec5171cba6a2225899849b836d0
Jarrod Gilliland <gillijar@gmail.com>
2021-07-22 11:20:18 -0400

    setup fetch request for meals from outside api

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,28 +1,59 @@
+import { useSelector } from "react-redux";
+import { useEffect, useState, useCallback } from "react";
+
 import MenuItems from "./MenuItems";
 import classes from "./Menu.module.css";
 
 const Menu = () => {
-  const DUMMY_ITEMS = [
-    { id: "m1", title: "Pizza", price: 19.99 },
-    { id: "m2", title: "Hamburger", price: 13 },
-  ];
+  const cartAmount = useSelector((state) => state.cart.totalQuantity);
+  const [menuItems, setMenuItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMenuData = useCallback(async () => {
+    setIsLoading(true);
+    const response = await fetch(
+      "https://react-ecommerce-17663-default-rtdb.firebaseio.com/meals.json"
+    );
+    const data = await response.json();
+
+    const transformedMenuData = [];
+
+    for (const key in data) {
+      const menuObj = {
+        key: data[key].id,
+        ...data[key],
+      };
+
+      transformedMenuData.push(menuObj);
+    }
+
+    setMenuItems(transformedMenuData);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMenuData();
+  }, [fetchMenuData]);
 
   return (
     <section className={classes.menu}>
       <div className={classes.menu__container}>
         <p className={classes["menu__container--cart-desc"]}>
-          You have 3 items in your cart
+          You have {cartAmount} items in your cart
         </p>
-        <ul>
-          {DUMMY_ITEMS.map((item) => (
-            <MenuItems
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-            />
-          ))}
-        </ul>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && (
+          <ul>
+            {menuItems.map((item) => (
+              <MenuItems
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+              />
+            ))}
+          </ul>
+        )}
       </div>
     </section>
   );
