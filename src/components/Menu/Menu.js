d4203bb417353c40997310c99d94e2822585cf7b
Jarrod Gilliland <gillijar@gmail.com>
2021-07-23 16:16:37 -0400

    refactored code

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, Fragment, useMemo } from "react";
+import { useEffect, useState, Fragment } from "react";
 
 import MenuItems from "./MenuItems";
 import LoadingSpinner from "../UI/LoadingSpinner";
@@ -9,42 +9,42 @@ const Menu = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
 
-  const fetchMenuData = useCallback(async () => {
-    setIsLoading(true);
-
-    try {
-      const response = await fetch(
-        "https://react-ecommerce-17663-default-rtdb.firebaseio.com/meals.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch the menu, please try again!");
-      }
-      const data = await response.json();
-
-      const transformedMenuData = [];
-
-      for (const key in data) {
-        const menuObj = {
-          key: data[key].id,
-          ...data[key],
-        };
-
-        transformedMenuData.push(menuObj);
-      }
-
-      setMenuItems(transformedMenuData);
-      setIsLoading(false);
-    } catch (err) {
-      console.log(err.message);
-      setHasError(true);
-      setIsLoading(false);
-    }
-  }, []);
-
   useEffect(() => {
+    const fetchMenuData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://react-ecommerce-17663-default-rtdb.firebaseio.com/meals.json"
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch the menu, please try again!");
+        }
+        const data = await response.json();
+
+        const transformedMenuData = [];
+
+        for (const key in data) {
+          const menuObj = {
+            key: data[key].id,
+            ...data[key],
+          };
+
+          transformedMenuData.push(menuObj);
+        }
+
+        setMenuItems(transformedMenuData);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setHasError(true);
+        setIsLoading(false);
+      }
+    };
+
     fetchMenuData();
-  }, [fetchMenuData]);
+  }, []);
 
   return (
     <section className={classes.menu}>
