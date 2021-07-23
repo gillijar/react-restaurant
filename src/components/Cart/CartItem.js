f5ae4d4fb7e746eb97b8e791b402ba4411d785f2
Jarrod Gilliland <gillijar@gmail.com>
2021-07-23 16:18:27 -0400

    added functionality for updating cart items

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,22 +1,43 @@
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-items";
+
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { id, name, quantity, totalPrice, price } = props;
+
+  const addItemToCart = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        name,
+        price,
+        totalPrice,
+        quantity,
+      })
+    );
+  };
+
   return (
-    <figure className={classes["cart-item"]}>
-      <div className={classes["cart-item__details"]}>
-        <p className={classes["cart-item__details--multi"]}>
-          x{props.quantity}
-        </p>
-        <p>{props.name}</p>
-      </div>
-      <div>
-        <div className={classes["cart-item__price"]}>
-          <p>${props.price}</p>
+    <li className={classes["cart-item"]}>
+      <figure>
+        <div className={classes["cart-item__details"]}>
+          <p className={classes["cart-item__details--multi"]}>
+            x{props.quantity}
+          </p>
+          <p>{props.name}</p>
         </div>
-        <button>-</button>
-        <button>+</button>
-      </div>
-    </figure>
+        <div>
+          <div className={classes["cart-item__price"]}>
+            <p>${props.totalPrice.toFixed(2)}</p>
+          </div>
+          <button>-</button>
+          <button onClick={addItemToCart}>+</button>
+        </div>
+      </figure>
+    </li>
   );
 };
 
