d3ea2ace56bb15ed2b030debc2a4e31c5084ba33
Jarrod Gilliland <gillijar@gmail.com>
2021-07-23 16:18:04 -0400

    refactored code

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,45 @@
 import { useSelector } from "react-redux";
+import { Fragment } from "react";
 
 import CartItem from "./CartItem";
+import GlobalButton from "../UI/GlobalButton";
 import classes from "./Cart.module.css";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
   const totalCartPrice = useSelector((state) => state.cart.totalCartPrice);
 
+  const cartItemContent = cartItems.map((item) => (
+    <CartItem
+      key={item.id}
+      id={item.id}
+      price={item.price}
+      name={item.name}
+      totalPrice={item.totalPrice}
+      quantity={item.quantity}
+    />
+  ));
+
   return (
     <section className={classes.cart}>
       <div className={classes["cart-container"]}>
         {cartItems.length === 0 && <p>Empty cart</p>}
-        {cartItems.length > 0 &&
-          cartItems.map((item) => (
-            <CartItem
-              name={item.name}
-              price={item.price}
-              quantity={item.quantity}
-            />
-          ))}
-        <p>${totalCartPrice.toFixed(2)}</p>
+        {cartItems.length > 0 && (
+          <Fragment>
+            <div className={classes["cart-container__total-price"]}>
+              <div>
+                <label htmlFor="total-cart-price>">Total:</label>
+                <p id="total-cart-price">${totalCartPrice.toFixed(2)}</p>
+              </div>
+            </div>
+            <ul>{cartItemContent}</ul>
+            <div className={classes["cart-container__btn"]}>
+              <GlobalButton userClass={classes["cart-container__checkout"]}>
+                Checkout
+              </GlobalButton>
+            </div>
+          </Fragment>
+        )}
       </div>
     </section>
   );
--- src/components/Menu/MenuItems.js
+++ src/components/Menu/MenuItems.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { cartActions } from "../../store/cart-items";
 
 import classes from "./MenuItems.module.css";
-import DefaultButton from "../UI/DefaultButton";
+import GlobalButton from "../UI/GlobalButton";
 
 const MenuItems = (props) => {
   const dispatch = useDispatch();
@@ -27,17 +27,12 @@ const MenuItems = (props) => {
         <p>${props.price}</p>
       </div>
       <div className={classes["menu-item__buttons"]}>
-        <Link className={classes["more-info"]}>
-          <DefaultButton userClass={classes["user-btn"]}>
-            More Info
-          </DefaultButton>
-        </Link>
-        <DefaultButton
+        <GlobalButton
           userClass={classes["user-btn"]}
           onClick={addToCartHandler}
         >
           Add To Cart
-        </DefaultButton>
+        </GlobalButton>
       </div>
     </li>
   );
