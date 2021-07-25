a80c0fd51afdde26db024cb54c87bd77b29b3e9c
Jarrod Gilliland <gillijar@gmail.com>
2021-07-25 19:35:02 -0400

    custom hook for form validity and error handling

---
+++ src/hooks/use-input.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+
+const initialInputState = { value: "", isTouched: false };
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { value: "", isTouched: false };
+  }
+  return initialInputState;
+};
+
+const useInput = (validateValue) => {
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+
+  const valueChangeHandler = (e) => {
+    dispatch({ type: "INPUT", value: e.target.value });
+  };
+
+  const valueBlurHandler = () => {
+    dispatch({ type: "BLUR" });
+  };
+
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+
+  return {
+    value: inputState.value,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    valueBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
