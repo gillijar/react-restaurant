081162fbcc93691a7edf7cee4537b0f66e5ae207
Jarrod Gilliland <gillijar@gmail.com>
2021-07-25 23:29:11 -0400

    added link around title to route to home page

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,14 @@
 import MainNavigation from "./MainNavigation";
+import { Link } from "react-router-dom";
 
 import classes from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <div className={classes["header__title"]}>Application Title</div>
+      <Link to="/home">
+        <div className={classes["header__title"]}>QuickEats</div>
+      </Link>
       <MainNavigation />
     </header>
   );
