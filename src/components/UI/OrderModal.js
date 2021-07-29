301c672323614c57499c60fb855226176cc018ef
Jarrod Gilliland <gillijar@gmail.com>
2021-07-29 19:46:49 -0400

    completed modal for when user submits order

--- src/components/UI/OrderModal.js
+++ src/components/UI/OrderModal.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
 import React, { Fragment } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../../store/cart-items";
+import { useHistory } from "react-router-dom";
 
 import classes from "./OrderModal.module.css";
 
@@ -9,22 +11,51 @@ const Backdrop = (props) => {
 };
 
 const ModalOverlay = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const orderNumber = Math.floor(Math.random() * 100 * Math.random() * 1000);
+
   const customerName = useSelector((state) => state.cart.customerName);
   const customerFirstName =
     customerName.split(" ")[0][0].toUpperCase() +
     customerName.split(" ")[0].slice(1).toLowerCase();
 
   const hour = new Date().getHours();
-  const minute = new Date().getMinutes();
-  console.log(hour, minute);
+  const minute = new Date().getMinutes() + 20;
+  const convertedHour = minute > 60 ? (hour + 1) % 12 : hour % 12;
+  const convertedMinute = String(minute % 60).padStart(2, 0);
+
+  const closeModal = () => {
+    dispatch(cartActions.returnOrderState());
+
+    history.push("/home");
+  };
 
   return (
-    <div className={classes.modal}>
-      <p>{/* Order Number: #<strong>{orderNumber}</strong> */}</p>
-      <p>
-        Thank you for your order, <strong>{customerFirstName}</strong>!
-      </p>
-      <p>Your food should be ready at approximately {}</p>
+    <div className={classes["modal-container"]}>
+      <div className={classes.modal}>
+        <div>
+          <p>
+            <i
+              className={`${classes["modal-icon"]} fas fa-times`}
+              onClick={closeModal}
+            ></i>
+          </p>
+        </div>
+        <p>
+          Order Number: <strong>#{orderNumber}</strong>
+        </p>
+        <p>
+          Thank you for your order, <strong>{customerFirstName}</strong>!
+        </p>
+        <p>
+          Your food should be ready at approximately{" "}
+          <strong>
+            {convertedHour}:{convertedMinute} {hour < 12 ? "AM" : "PM"}
+          </strong>
+        </p>
+      </div>
     </div>
   );
 };
