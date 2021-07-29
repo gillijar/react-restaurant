93b5508c6c56b6bce6007ae4993e01f02d4c4039
Jarrod Gilliland <gillijar@gmail.com>
2021-07-29 12:20:42 -0400

    use order modal inside of cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Fragment, useState } from "react";
+import { cartActions } from "../../store/cart-items";
 
 import CartItem from "./CartItem";
 import GlobalButton from "../UI/GlobalButton";
 import CartForm from "./CartForm";
+import OrderModal from "../UI/OrderModal";
 import classes from "./Cart.module.css";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+
   const cartItems = useSelector((state) => state.cart.items);
   const totalCartPrice = useSelector((state) => state.cart.totalCartPrice);
+  const orderSummary = useSelector((state) => state.cart.ordered);
 
   const [checkoutReady, setCheckoutReady] = useState(false);
 
@@ -27,41 +32,50 @@ const Cart = () => {
     setCheckoutReady((prevState) => !prevState);
   };
 
+  const closeModal = () => {
+    dispatch(cartActions.returnOrderState());
+  };
+
   return (
-    <section className={classes.cart}>
-      <div className={classes["cart-container"]}>
-        {cartItems.length === 0 && (
-          <p className={classes["empty-cart"]}>Your cart is currently empty.</p>
-        )}
-        {cartItems.length > 0 && (
-          <Fragment>
-            <div className={classes["cart-container__total-price"]}>
-              <div>
-                <label htmlFor="total-cart-price>">Total:</label>
-                <p id="total-cart-price">${totalCartPrice.toFixed(2)}</p>
+    <Fragment>
+      {orderSummary && <OrderModal onClose={closeModal} />}
+      <section className={classes.cart}>
+        <div className={classes["cart-container"]}>
+          {cartItems.length === 0 && (
+            <p className={classes["empty-cart"]}>
+              Your cart is currently empty.
+            </p>
+          )}
+          {cartItems.length > 0 && (
+            <Fragment>
+              <div className={classes["cart-container__total-price"]}>
+                <div>
+                  <label htmlFor="total-cart-price>">Total:</label>
+                  <p id="total-cart-price">${totalCartPrice.toFixed(2)}</p>
+                </div>
               </div>
-            </div>
-            <ul>{cartItemContent}</ul>
-            {!checkoutReady && (
-              <div className={classes["cart-container__btn"]}>
-                <GlobalButton
-                  onClick={readyToCheckout}
-                  userClass={classes["cart-container__checkout"]}
-                >
-                  Checkout
-                </GlobalButton>
-              </div>
-            )}
-            {checkoutReady && (
-              <CartForm
-                btnClass={classes["cart-container__btn"]}
-                cancelCheckout={readyToCheckout}
-              />
-            )}
-          </Fragment>
-        )}
-      </div>
-    </section>
+              <ul>{cartItemContent}</ul>
+              {!checkoutReady && (
+                <div className={classes["cart-container__btn"]}>
+                  <GlobalButton
+                    onClick={readyToCheckout}
+                    userClass={classes["cart-container__checkout"]}
+                  >
+                    Checkout
+                  </GlobalButton>
+                </div>
+              )}
+              {checkoutReady && (
+                <CartForm
+                  btnClass={classes["cart-container__btn"]}
+                  cancelCheckout={readyToCheckout}
+                />
+              )}
+            </Fragment>
+          )}
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
