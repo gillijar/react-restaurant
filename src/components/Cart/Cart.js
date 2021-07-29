dfea3fe70031c6553fe3800c90fcde21c2b5beb2
Jarrod Gilliland <gillijar@gmail.com>
2021-07-29 19:46:18 -0400

    added useHistory to redirect to home page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Fragment, useState } from "react";
 import { cartActions } from "../../store/cart-items";
+import { useHistory } from "react-router-dom";
 
 import CartItem from "./CartItem";
 import GlobalButton from "../UI/GlobalButton";
@@ -10,6 +11,7 @@ import classes from "./Cart.module.css";
 
 const Cart = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const cartItems = useSelector((state) => state.cart.items);
   const totalCartPrice = useSelector((state) => state.cart.totalCartPrice);
@@ -34,46 +36,50 @@ const Cart = () => {
 
   const closeModal = () => {
     dispatch(cartActions.returnOrderState());
+
+    history.push("/home");
   };
 
   return (
     <Fragment>
       {orderSummary && <OrderModal onClose={closeModal} />}
       <section className={classes.cart}>
-        <div className={classes["cart-container"]}>
-          {cartItems.length === 0 && (
-            <p className={classes["empty-cart"]}>
-              Your cart is currently empty.
-            </p>
-          )}
-          {cartItems.length > 0 && (
-            <Fragment>
-              <div className={classes["cart-container__total-price"]}>
-                <div>
-                  <label htmlFor="total-cart-price>">Total:</label>
-                  <p id="total-cart-price">${totalCartPrice.toFixed(2)}</p>
+        {!orderSummary && (
+          <div className={classes["cart-container"]}>
+            {cartItems.length === 0 && (
+              <p className={classes["empty-cart"]}>
+                Your cart is currently empty.
+              </p>
+            )}
+            {cartItems.length > 0 && (
+              <Fragment>
+                <div className={classes["cart-container__total-price"]}>
+                  <div>
+                    <label htmlFor="total-cart-price>">Total:</label>
+                    <p id="total-cart-price">${totalCartPrice.toFixed(2)}</p>
+                  </div>
                 </div>
-              </div>
-              <ul>{cartItemContent}</ul>
-              {!checkoutReady && (
-                <div className={classes["cart-container__btn"]}>
-                  <GlobalButton
-                    onClick={readyToCheckout}
-                    userClass={classes["cart-container__checkout"]}
-                  >
-                    Checkout
-                  </GlobalButton>
-                </div>
-              )}
-              {checkoutReady && (
-                <CartForm
-                  btnClass={classes["cart-container__btn"]}
-                  cancelCheckout={readyToCheckout}
-                />
-              )}
-            </Fragment>
-          )}
-        </div>
+                <ul>{cartItemContent}</ul>
+                {!checkoutReady && (
+                  <div className={classes["cart-container__btn"]}>
+                    <GlobalButton
+                      onClick={readyToCheckout}
+                      userClass={classes["cart-container__checkout"]}
+                    >
+                      Checkout
+                    </GlobalButton>
+                  </div>
+                )}
+                {checkoutReady && (
+                  <CartForm
+                    btnClass={classes["cart-container__btn"]}
+                    cancelCheckout={readyToCheckout}
+                  />
+                )}
+              </Fragment>
+            )}
+          </div>
+        )}
       </section>
     </Fragment>
   );
