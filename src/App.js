ed69e61faec06de4a0b4a8fe4e3a88ce6b0a3117
Jarrod Gilliland <gillijar@gmail.com>
2021-08-29 22:29:49 -0400

    moved fetch menu to app instead of menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Switch, Route, Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { menuItemsActions } from "./store/menu-items";
 
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
@@ -9,6 +12,53 @@ import PageNotFound from "./pages/PageNotFound";
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const [menuItems, setMenuItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchMenuData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://react-ecommerce-17663-default-rtdb.firebaseio.com/meals.json"
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch the menu, please try again!");
+        }
+        const data = await response.json();
+
+        const transformedMenuData = [];
+
+        for (const key in data) {
+          const menuObj = {
+            key: data[key].id,
+            ...data[key],
+          };
+
+          transformedMenuData.push(menuObj);
+        }
+
+        setMenuItems(transformedMenuData);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setHasError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchMenuData();
+  }, []);
+
+  useEffect(() => {
+    dispatch(menuItemsActions.addMenuItems(menuItems));
+  }, [menuItems, dispatch]);
+
   return (
     <Layout>
       <Switch>
@@ -19,7 +69,7 @@ function App() {
           <HomePage />
         </Route>
         <Route path="/menu" exact>
-          <MenuPage />
+          <MenuPage isLoading={isLoading} hasError={hasError} />
         </Route>
         <Route path="/menu/:itemId">
           <MenuItemDetail />
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,69 +1,23 @@
-import { useEffect, useState, Fragment } from "react";
-import { useDispatch } from "react-redux";
-import { menuItemsActions } from "../../store/menu-items";
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 
 import MenuItems from "./MenuItems";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./Menu.module.css";
 
-const Menu = () => {
-  const dispatch = useDispatch();
-
-  const [menuItems, setMenuItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(false);
-
-  useEffect(() => {
-    const fetchMenuData = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(
-          "https://react-ecommerce-17663-default-rtdb.firebaseio.com/meals.json"
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch the menu, please try again!");
-        }
-        const data = await response.json();
-
-        const transformedMenuData = [];
-
-        for (const key in data) {
-          const menuObj = {
-            key: data[key].id,
-            ...data[key],
-          };
-
-          transformedMenuData.push(menuObj);
-        }
-
-        setMenuItems(transformedMenuData);
-        setIsLoading(false);
-      } catch (err) {
-        console.log(err.message);
-        setHasError(true);
-        setIsLoading(false);
-      }
-    };
-
-    fetchMenuData();
-  }, []);
-
-  useEffect(() => {
-    dispatch(menuItemsActions.addMenuItems(menuItems));
-  }, [menuItems, dispatch]);
+const Menu = (props) => {
+  const menuItems = useSelector((state) => state.menuItems.items);
 
   return (
     <section className={classes.menu}>
       <div className={classes["menu__container"]}>
-        {isLoading && <LoadingSpinner />}
-        {hasError && (
+        {props.isLoading && <LoadingSpinner />}
+        {props.hasError && (
           <p className={classes.error}>
             Failed to fetch menu data. Please try again!
           </p>
         )}
-        {!isLoading && !hasError && (
+        {!props.isLoading && !props.hasError && (
           <Fragment>
             <h2>Our Menu</h2>
             <ul>
