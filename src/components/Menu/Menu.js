9293022b7dbaaa22e5c57d29d5859efcf2f651cf
Jarrod Gilliland <gillijar@gmail.com>
2021-09-01 10:54:54 -0400

    removed unused components

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 
 import MenuItems from "./MenuItems";
 import MenuNav from "./MenuNav";
-import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./Menu.module.css";
 
 const Menu = (props) => {
@@ -16,27 +15,19 @@ const Menu = (props) => {
     <section className={classes.menu}>
       <MenuNav />
       <div className={classes["menu__container"]}>
-        {props.isLoading && <LoadingSpinner />}
-        {props.hasError && (
-          <p className={classes.error}>
-            Failed to fetch menu data. Please try again!
-          </p>
-        )}
-        {!props.isLoading && !props.hasError && (
-          <Fragment>
-            <ul>
-              {menuItems.map((item) => (
-                <MenuItems
-                  key={item.id}
-                  id={item.id}
-                  title={item.title}
-                  price={item.price}
-                  description={item.description}
-                />
-              ))}
-            </ul>
-          </Fragment>
-        )}
+        <Fragment>
+          <ul>
+            {menuItems.map((item) => (
+              <MenuItems
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                description={item.description}
+              />
+            ))}
+          </ul>
+        </Fragment>
       </div>
     </section>
   );
